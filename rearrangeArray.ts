b34a0bb3b8b2e4eb6dc4a9dcd1e0a08e677194e4
Thar Lin Shwe <albent2017@gmail.com>
2024-02-14 13:10:52 +0800

    Optimized rearrangeArray

--- rearrangeArray.ts
+++ rearrangeArray.ts
@@ -1,39 +1,29 @@
 function rearrangeArray(nums: number[]): number[] {
-  divideArrayIntoPositiveAndNegative(nums);
-
-  const pivot = findPivot(nums);
-  const result: number[] = [];
-
-  for (let i = 0; i < pivot; i++) {
-    result.push(nums[i]);
-    result.push(nums[pivot + i]);
-  }
-
-  return result;
-}
-
-function swap<T>(nums: Array<T>, a: number, b: number) {
-  [nums[b], nums[a]] = [nums[a], nums[b]];
-}
-
-function findPivot(nums: number[]) {
-  return nums.findIndex((val) => val < 0);
-}
-
-function divideArrayIntoPositiveAndNegative(nums: number[]) {
+  const [positive, negative] = divideArrayIntoPositiveAndNegative(nums);
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0) {
-      let count = i;
-      while (nums[count] < 0) {
-        if (count >= nums.length - 1) {
-          return;
-        }
-        count++;
-      }
-
-      while (count != i) {
-        swap(nums, count--, count);
-      }
+    if (i % 2 == 0) {
+      nums[i] = positive[i / 2];
+    } else {
+      nums[i] = negative[Math.floor(i / 2)];
     }
   }
+  return nums;
 }
+
+function divideArrayIntoPositiveAndNegative(nums: number[]): number[][] {
+  const positive = new Array(nums.length / 2).fill(0);
+  const negative = new Array(nums.length / 2).fill(0);
+  let negativePointer = 0;
+  let positivePointer = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < 0) {
+      negative[negativePointer++] = nums[i];
+    } else {
+      positive[positivePointer++] = nums[i];
+    }
+  }
+  return [positive, negative];
+}
+
+const nums = [-1, 2, -3, -5, 2, 1];
+console.log(rearrangeArray(nums));

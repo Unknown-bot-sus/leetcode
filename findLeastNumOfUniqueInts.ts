6102748c9bd4b6e396f30068820f2790b61456e6
Thar Lin Shwe <albent2017@gmail.com>
2024-02-16 13:42:23 +0800

    Optimized findLeastNumOfUniqueInts

--- findLeastNumOfUniqueInts.ts
+++ findLeastNumOfUniqueInts.ts
@@ -5,22 +5,12 @@ function findLeastNumOfUniqueInts(arr: number[], k: number): number {
     counter.set(num, (counter.get(num) ?? 0) + 1);
   }
 
-  const set = new Set(arr);
-
-  const sortedCount = [...counter].sort((a, b) => a[1] - b[1]);
-
-  for (let i = 0; i < k; ) {
-    const row = sortedCount[i];
-
-    if (row[1] === 1) {
-      set.delete(row[0]);
-
-      ++i;
-    } else {
-      row[1] = row[1] - 1;
-      --k;
-    }
+  const frequencies = [...counter.values()].sort((a, b) => a - b);
+  for (const [index, count] of frequencies.entries()) {
+    console.log(k, count);
+    if (k >= count) k = k - count;
+    else return frequencies.length - index;
   }
 
-  return set.size;
+  return 0;
 }

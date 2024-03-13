afc35dbfead56e485d99c019eb8c43cdc71aeca5
Thar Lin Shwe <albent2017@gmail.com>
2024-03-13 12:00:44 +0800

    pivotInteger optimized

--- pivotInteger.ts
+++ pivotInteger.ts
@@ -1,17 +1,16 @@
 function pivotInteger(n: number): number {
-  const array = Array.from({ length: n + 1 }, (_, index) => index);
   let left = 0,
-    right = array.length - 1;
-  let prefixSum = array[left],
-    postfixSum = array[right];
+    right = n;
+  let prefixSum = left,
+    postfixSum = right;
 
   while (left !== right) {
     if (prefixSum < postfixSum) {
-      prefixSum += array[++left];
+      prefixSum += ++left;
     } else if (postfixSum < prefixSum) {
-      postfixSum += array[--right];
+      postfixSum += --right;
     } else {
-      prefixSum += array[++left];
+      prefixSum += ++left;
     }
   }
   return prefixSum === postfixSum ? left : -1;
